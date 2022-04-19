0b4d8bc8ac1b769337d2844ea01533630998eab9
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-04-19 16:58:33 +0530

    Add files via upload

---
+++ LearnMethods.java
@@ -0,0 +1,67 @@
+package Week1.Day2;
+
+public class LearnMethods {
+
+	public static void main(String[] args) {
+	
+		LearnMethods LM = new LearnMethods();
+		LM.getCarName();
+		
+		
+		
+		int getCarRegNumber = LM.getCarRegNumber();
+		System.out.print(getCarRegNumber);
+		
+		stringgetcarVarient=LM.getCarRegNumbert();
+		System.out.print(getcarRegNumber);
+		
+		int multiplyTwoNumbers = LM.multiplyTwoNumbers(3,6);
+		
+		System.out.print(multiplyTwoNumbers);
+		
+	}
+	
+	
+	public void getCarName()
+	{
+		
+	system.out.print("MaruthiDzire");
+		
+	}
+	
+	public int getCarRegNumber()
+	{
+		int Regnumber = 5587;
+		
+		return Regnumber;
+		
+	}
+	
+	public String getcarVarient()
+	{
+		return "maruthiDzire";
+		
+	}
+	
+	public int multiplyTwoNumbers (int a, int b)
+	{
+		
+		
+		return a*b;
+
+		
+
+				
+}				
+				
+	
+		
+		
+	
+	
+	
+	}
+		
+		
+		
+	}
---
+++ PositiveAndNegative.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class PositiveAndNegative {
+	public static void main(String[] args) {
+		int i = 23;
+		if(i>0) {
+			system.out.print1n("the number is positive");
+			
+		}
+		else if (i<0)
+		{
+			System.out.print1n("the number is negative");
+		}
+		else
+			}
+           system.out.print1n("the number is neither negative or positive");
+}        
+}
