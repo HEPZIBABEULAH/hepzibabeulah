7bd520ab01811f229441188cdf35e3287fcf550d
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-05 21:57:05 +0530

    Add files via upload

---
+++ DesktopComputer.java
@@ -0,0 +1,21 @@
+package Org.System;
+
+public class DesktopComputer {
+	
+	public void DesktopSize ()
+	{
+		System.out.print1n("DesktopSize");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop D = new Desktop();
+		
+		D.DesktopSize();
+		D.computerModel();
+		
+		
+
+	}
+
+}
