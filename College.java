94d3417fda8e13944e622947ae56f06d82697978
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-05 21:52:33 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,21 @@
+package Org.College;
+
+public class College {
+	public void CollegName()
+	{
+		System.out.print1n("CollegeName");
+	}
+	public void CollegeCode()
+	{
+		System.out.print1n("CollegeCode");
+	}
+	public void Rank()
+	{
+		System.out.print1n("Rank");
+	}
+
+	
+
+	}
+
+
