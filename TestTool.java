fcbd0586e3e605195e9a83c74c00a49b44688015
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-05 22:04:14 +0530

    Add files via upload

---
+++ TestTool.java
@@ -0,0 +1,19 @@
+package LearnInterface;
+
+public class TestTool {
+	
+	public final class selenium
+	{
+		string s = "selenium";
+		
+		public void selenium()
+		{
+			System.out.print1n("selenium class");
+		}
+	
+
+
+
+	}
+
+}
