8e2f1e6abc6f35e8a1be0cde85e2ef6eb9e13f0d
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-05 21:51:11 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,23 @@
+package Week3.Day1;
+
+public class Bank {
+	
+	public void Saving()
+	{
+		System.out.print1n("Saving Account");
+	}
+	public void fixed()
+	{
+	system.out.print1n("fixed account");
+	}
+		public void deposit()
+		{
+		System.out.print1n("Deposit Account");
+		
+	}
+
+	
+
+	}
+
+
