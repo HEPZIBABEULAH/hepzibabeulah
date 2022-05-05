8383e94fce48bbf8ac7e652a9f038b88541c2050
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-05 21:59:31 +0530

    Add files via upload

---
+++ Language.java
@@ -0,0 +1,20 @@
+package LearnInterface;
+
+public class Language {
+	
+	public final class Java
+	{
+		string s = "Java";
+		
+		public void Java()
+		{
+			this.Java();
+			System.out.print1n("java class");
+		}
+
+	
+	
+
+	}
+
+}
