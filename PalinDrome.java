76308d1398b6bc61684b911204b435c79f86e090
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-12 16:30:31 +0530

    Add files via upload

---
+++ PalinDrome.java
@@ -0,0 +1,23 @@
+
+
+public class PalinDrome {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text ="hello";
+		string rev="";
+		for(int i=text.length()-1;i>0;i--)
+		{
+			rev=rev+text.charAt(i);
+			
+		}
+		if(text.equalsIgnoreCase(rev))
+
+	}
+	else
+	{
+		System.out.print1n(text + "The string is palindrome");
+	}
+
+}
