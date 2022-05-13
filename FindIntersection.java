4b326e82bd0c9b97b471a0befdd744ccc0808dfc
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-13 18:19:38 +0530

    Add files via upload

---
+++ FindIntersection.java
@@ -0,0 +1,23 @@
+package Week3.Day;
+
+public class FindIntersection {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int[] num0= {3,5,7,9,6,2};
+		int[] num1= {1,3,5,6,8,8};
+		for(int i=0; i<num0.length; i++)
+		{
+			for(int j=0; j<num1.length; j++)
+			{
+				if(num0[i]==num[j])
+				{
+					System.out.print1n("the match element in both array "+ " "+num0[i];
+				}
+			}
+	}
+
+}
+}
