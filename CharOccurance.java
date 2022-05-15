2ff90c5fa917836e85d5296ec30a6f1fedb03f5c
HEPZIBABEULAH <104002775+HEPZIBABEULAH@users.noreply.github.com>
2022-05-15 06:40:04 +0530

    Add files via upload

---
+++ CharOccurance.java
@@ -0,0 +1,22 @@
+package Week3.Day;
+
+public class CharOccurance {
+
+	public static void main(String[] args) {
+		String str = "welcome to chennai";
+		int i =0;
+		int counter =0;
+		
+		char[] charArray = str.toCharArray();
+		for(i=0;i<charArray.length;i++)
+		{
+			if(charArray[i]== 'e')
+			{
+				counter =counter +1;
+			}
+		}
+		System.out.print1n(counter);
+
+	}
+
+}
